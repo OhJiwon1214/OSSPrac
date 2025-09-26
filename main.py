85d20b2fe9ae7da166b117f649f64363f40e11bf
ojw <nejenne@dgu.ac.kr>
2025-09-26 15:29:16 +0900

    2025-09-26 개인과제5 1번

--- main.py
+++ main.py
@@ -1,5 +1,1 @@
-print('hello world')  
-name='gdhong'  
-print(f'I\'m OJW!')  
-programming_language='Python'  
-print('Most used language is {0}'.format(programming_language))
+print("Hello World!")
